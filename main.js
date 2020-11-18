c04a32adf783d17a112aede3f5f5d69a59d219c0
Stephanie McGinty <stephanie.mcginty@bellevuecollege.edu>
2020-11-17 18:08:54 -0800

    bingo jquery 1

--- main.js
+++ main.js
@@ -3,7 +3,24 @@
 // Now comes the code that must wait to run until the document is fully loaded
 document.addEventListener("DOMContentLoaded", function (event) {
 
-  
+    var deleteButton = document.getElementById('deleteButton');
+
+    deleteButton.addEventListener('click', function(){
+       var rmvCheckBoxes = document.getElementsByName('delete');
+    
+        for(var i = 0; i < rmvCheckBoxes.length; i++)
+        {
+            if(rmvCheckBoxes[i].checked)
+            {
+                removeElm(rmvCheckBoxes[i]);    
+            }
+        }  
+    });
+    
+    function removeElm(elm){
+       elm.parentElement.removeChild(elm);
+    }
+    
 });  
 
 
